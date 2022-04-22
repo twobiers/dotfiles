18cdee0185aa34b8e51731b9420a4aeccac7d813
twobiers <22715034+twobiers@users.noreply.github.com>
2022-04-22 22:50:06 +0200

    adjust

--- dot_config/powershell/aliases.ps1
+++ dot_config/powershell/aliases.ps1
@@ -12,4 +12,4 @@ ${function:drop} = { Set-Location ~\Documents\Dropbox }
 ${function:dt} = { Set-Location ~\Desktop }
 ${function:docs} = { Set-Location ~\Documents }
 ${function:dl} = { Set-Location ~\Downloads }
-${function:prj} = { Set-Location ~\Documents\Projekte }
+${function:prj} = { Set-Location P: }
