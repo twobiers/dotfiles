b0d6c370940a00b5f57787284e80acad6458ef51
twobiers <22715034+twobiers@users.noreply.github.com>
2022-04-24 18:51:28 +0200

    update profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,16 @@
 # Set-Theme Paradox
 # oh-my-posh --init --shell pwsh --config "~/iterm3.omp.json" | Invoke-Expression
 
+function Invoke-Starship-PreCommand {
+    $loc = $($executionContext.SessionState.Path.CurrentLocation);
+    $prompt = "$([char]27)]9;12$([char]7)"
+    if ($loc.Provider.Name -eq "FileSystem")
+    {
+      $prompt += "$([char]27)]9;9;`"$($loc.Path)`"$([char]7)"
+    }
+    $host.ui.Write($prompt)
+}
+
 try {
     if(Get-Command starship) {
         Invoke-Expression (&starship init powershell)
@@ -16,7 +26,7 @@ Catch {
 . $HOME/.config/powershell/aliases.ps1
 
 # Register Autocompletions
-$completions = @("chezmoi", "dotnet", "kubectl")
+$completions = @("chezmoi", "dotnet", "kubectl", "choco")
 foreach ($script in $completions) {
     . $HOME/.config/powershell/completions/$script.ps1
 }
