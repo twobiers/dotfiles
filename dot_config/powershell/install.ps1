067c5156124b6e130ed9de3b2ca9e2492c5a42e1
twobiers <22715034+twobiers@users.noreply.github.com>
2022-07-14 19:44:49 +0200

    fix ssh/gpg

--- dot_config/powershell/install.ps1
+++ dot_config/powershell/install.ps1
@@ -2,7 +2,7 @@
 
 $decision = $Host.UI.PromptForChoice("Install", "This script is meant to bootstrap a windows environemnt. Are you sure you want to continue?", @('&Yes', '&No'), 1)
 if ($decision -ne 0) {
-    exit
+	exit
 }
 
 Write-Host "Updating Help..." -ForegroundColor "Yellow"
@@ -13,9 +13,9 @@ Install-Module Posh-Git -Scope CurrentUser -Force
 
 Write-Host "Installing Choco..." -ForegroundColor "Yellow"
 if ($null -eq (Get-Command cinst -ErrorAction SilentlyContinue | Select-Object Definition)) {
-    Invoke-Expression (new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')
-    Refresh-Environment
-    choco feature enable -n=allowGlobalConfirmation
+	Invoke-Expression (new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')
+	Refresh-Environment
+	choco feature enable -n=allowGlobalConfirmation
 }
 
 Write-Host "Installing Choco packages..." -ForegroundColor "Yellow"
@@ -85,16 +85,16 @@ Install-Module -Name PSFzf
 Write-Host "Installing winget..." -ForegroundColor "Yellow"
 $hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
 if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
-    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+	Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
 
-    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+	$releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
 
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $releases = Invoke-RestMethod -uri $releases_url
-    $latestRelease = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	$releases = Invoke-RestMethod -uri $releases_url
+	$latestRelease = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
 
-    "Installing winget from $($latestRelease.browser_download_url)"
-    Add-AppxPackage -Path $latestRelease.browser_download_url
+	"Installing winget from $($latestRelease.browser_download_url)"
+	Add-AppxPackage -Path $latestRelease.browser_download_url
 }
 
 Write-Host "Installing winget packages..." -ForegroundColor "Yellow"
@@ -110,16 +110,16 @@ Invoke-WebRequest -Uri $Uri -OutFile $Location
 $DestinationPath = "$HOME\Downloads\JetBrainsMono"
 Expand-Archive -Path $Location -DestinationPath $DestinationPath -Force
 # Based on this gist: https://gist.github.com/anthonyeden/0088b07de8951403a643a8485af2709b?permalink_comment_id=3651336#gistcomment-3651336
-foreach($FontFile in Get-ChildItem $OutputFilename -Include '*.ttf','*.ttc','*.otf' -recurse ) {
+foreach ($FontFile in Get-ChildItem $OutputFilename -Include '*.ttf', '*.ttc', '*.otf' -recurse ) {
 	$targetPath = Join-Path $SystemFontsPath $FontFile.Name
-	if(!(Test-Path -Path $targetPath)) {
+	if (!(Test-Path -Path $targetPath)) {
 		#Extract Font information for Reqistry 
 		$ShellFolder = (New-Object -COMObject Shell.Application).Namespace($fontSourceFolder)
 		$ShellFile = $ShellFolder.ParseName($FontFile.name)
 		$ShellFileType = $ShellFolder.GetDetailsOf($ShellFile, 2)
 
 		#Set the $FontType Variable
-		If ($ShellFileType -Like '*TrueType font file*') {$FontType = '(TrueType)'}
+		If ($ShellFileType -Like '*TrueType font file*') { $FontType = '(TrueType)' }
 			
 		#Update Registry and copy font to font directory
 		$RegName = $ShellFolder.GetDetailsOf($ShellFile, 21) + ' ' + $FontType
@@ -139,7 +139,7 @@ $pageantPath = "$pageantDir\wsl-ssh-pageant-amd64-gui.exe"
 $pageantSockPath = "$pageantDir\wsl-ssh-agent.sock"
 $pageantPipeName = "winssh-pageant"
 $autostartDir = "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-If(!(Test-Path $pageantDir)) {
+If (!(Test-Path $pageantDir)) {
 	New-Item -ItemType Directory -Force -Path $pageantDir
 }
 Invoke-WebRequest -Uri "https://github.com/benpye/wsl-ssh-pageant/releases/download/20201121.2/wsl-ssh-pageant-amd64-gui.exe" -OutFile $pageantPath
@@ -155,6 +155,17 @@ Write-Output "PowerShell.exe -WindowStyle Hidden -Command 'gpg-connect-agent /by
 # Import public GPG key from github
 Invoke-WebRequest https://github.com/twobiers.gpg | gpg --import
 
+#####################
+# npiperelay
+#####################
+$npiperelayDir = "C:\tools\npiperelay"
+$npiperelayPath = "$npiperelayDir\npiperelay.exe"
+If (!(Test-Path $npiperelayDir)) {
+	New-Item -ItemType Directory -Force -Path $npiperelayDir
+}
+Invoke-WebRequest -Uri "https://github.com/NZSmartie/npiperelay/releases/download/v0.1/npiperelay.exe" -OutFile $npiperelayPath
+
+
 ################
 # WSL 2
 ################
@@ -165,5 +176,5 @@ wsl --install -d Ubuntu
 
 $decision = $Host.UI.PromptForChoice("Install", "Base instllation finished, do you also want to apply windows defaults?", @('&Yes', '&No'), 1)
 if ($decision -eq 0) {
-    . ./windows.ps1
+	. ./windows.ps1
 }
