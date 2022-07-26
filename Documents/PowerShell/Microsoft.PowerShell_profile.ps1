4ccc7cba1d1608c7952bfb278fc0aeb478e93378
twobiers <22715034+twobiers@users.noreply.github.com>
2022-07-26 10:24:19 +0200

    tweak options for opening new shell on same path

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,18 +4,18 @@
 # oh-my-posh --init --shell pwsh --config "~/iterm3.omp.json" | Invoke-Expression
 
 if ($host.name -eq "ConsoleHost") {
+    # TODO: This is relatively slow
     function Invoke-Starship-PreCommand {
-        $loc = $($executionContext.SessionState.Path.CurrentLocation);
+        $loc = Get-Location
         $prompt = "$([char]27)]9;12$([char]7)"
-        if ($loc.Provider.Name -eq "FileSystem")
-        {
-        $prompt += "$([char]27)]9;9;`"$($loc.Path)`"$([char]7)"
+        if ($loc.Provider.Name -eq "FileSystem") {
+            $prompt += "$([char]27)]9;9;`"$($loc.Path)`"$([char]7)"
         }
         $host.ui.Write($prompt)
     }
 
     try {
-        if(Get-Command starship) {
+        if (Get-Command starship) {
             Invoke-Expression (&starship init powershell)
         }
     }
@@ -24,11 +24,11 @@ if ($host.name -eq "ConsoleHost") {
     }
 
     try {
-        if(Get-Command zoxide) {
+        if (Get-Command zoxide) {
             Invoke-Expression (& {
-                $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+                    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
                 (zoxide init --hook $hook powershell | Out-String)
-            })            
+                })            
         }
     }
     Catch {
@@ -41,12 +41,13 @@ if ($host.name -eq "ConsoleHost") {
         try {
             Import-Module $mod
 
-            if($mod -eq "PSFzf") {
-            # Don't replace tab completion for the moment. I don't like it. PSReadline works like a charm for me
-            #    Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+            if ($mod -eq "PSFzf") {
+                # Don't replace tab completion for the moment. I don't like it. PSReadline works like a charm for me
+                #    Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
                 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
             }
-        } catch {
+        }
+        catch {
             Write-Host "Module $mod is not installed, run";
             Write-Host -ForegroundColor yellow "Install-Module -Name $mod -AllowPrerelease"
             Write-Host "to install"
@@ -59,9 +60,10 @@ if ($host.name -eq "ConsoleHost") {
     # Register Autocompletions
     $completions = @("chezmoi", "dotnet", "kubectl", "choco", "kubefwd", "docker", "zoxide", "winget")
     foreach ($script in $completions) {
-        if(Get-Command $script -ErrorAction SilentlyContinue) {
+        if (Get-Command $script -ErrorAction SilentlyContinue) {
             . $HOME/.config/powershell/completions/$script.ps1
-        } else {
+        }
+        else {
             Write-Host -ForegroundColor Yellow "Could not find $script installed, but a completion script is registered. Skipping it...."
         }
     }
