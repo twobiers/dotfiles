3444b8f7dcdafaf53295b693437dd473ad25e27e
twobiers <22715034+twobiers@users.noreply.github.com>
2022-10-09 17:31:43 +0200

    tweak

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -25,6 +25,7 @@ fi
 # Use fd if installed
 if [[ -x fd ]]; then
   export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --color=always'
-  export FZF_DEFAULT_OPTS="--ansi"
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
+
+export FZF_DEFAULT_OPTS="--ansi --height 40% --border"
