98c7d0db62b9f426cb4ba3967a1a08ce43b37f2f
twobiers <22715034+twobiers@users.noreply.github.com>
2022-10-09 17:07:31 +0200

    update fzf config

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -21,3 +21,10 @@ fi
 if test -f "/home/tobi/.fzf/shell/key-bindings.zsh"; then
   source "/home/tobi/.fzf/shell/key-bindings.zsh"
 fi
+
+# Use fd if installed
+if [[ -x fd ]]; then
+  export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --color=always'
+  export FZF_DEFAULT_OPTS="--ansi"
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
