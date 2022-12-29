f3b67237418841e2fc7e4c17e2b450532a9efbae
twobiers <22715034+twobiers@users.noreply.github.com>
2022-12-29 17:40:20 +0100

    add fzf-tab

--- dot_config/zsh/rc.d/05-plugins.zsh
+++ dot_config/zsh/rc.d/05-plugins.zsh
@@ -4,17 +4,20 @@ znap source ohmyzsh/ohmyzsh \
   lib/{clipboard,compfix,completion,correction,directories,git,grep,history,spectrum,termsupport} \
   plugins/{fzf,git,gpg-agent}
 
+znap source zsh-users/zsh-completions
+
 znap source chrissicool/zsh-256color
 znap source zdharma-continuum/fast-syntax-highlighting
-znap source zsh-users/zsh-completions
 
 ZSH_AUTOSUGGEST_STRATEGY=(history)
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=240'
 znap source zsh-users/zsh-autosuggestions
 
-zstyle ':autocomplete:*' fzf-completion yes
-zstyle ':autocomplete:recent-dirs' backend zoxide
-znap source marlonrichert/zsh-autocomplete
+# zstyle ':autocomplete:*' fzf-completion yes
+# zstyle ':autocomplete:recent-dirs' backend zoxide
+# znap source marlonrichert/zsh-autocomplete
 znap eval trapd00r/LS_COLORS "$(whence -a dircolors gdircolors) -b LS_COLORS"
 znap source unixorn/fzf-zsh-plugin
+
+znap source fzf-tab
