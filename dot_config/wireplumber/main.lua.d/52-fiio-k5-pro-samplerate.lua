346e0eeb08b347ffa6147e08b305529f60c88fdd
twobiers <22715034+twobiers@users.noreply.github.com>
2022-12-29 14:41:30 +0100

    add pipewire/wireplumber settings

---
+++ dot_config/wireplumber/main.lua.d/52-fiio-k5-pro-samplerate.lua
@@ -0,0 +1,13 @@
+rule = {
+	matches = {
+		{
+			{ "node.name", "matches", "alsa_output.usb-GuangZhou_FiiO_Electronics_Co._Ltd_FiiO_K5_Pro-00.iec958-stereo" },
+		},
+	},
+	apply_properties = {
+		["audio.format"] = "S32_LE",
+		["audio.rate"] = 41000
+	},
+}
+
+table.insert(alsa_monitor.rules, rule)
