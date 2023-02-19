0bd609ec47d364d9dba319d5dd5aaa90cd4975d3
twobiers <22715034+twobiers@users.noreply.github.com>
2023-02-19 20:01:43 +0100

    update

--- dot_config/zsh/rc.d/05-plugins.zsh
+++ dot_config/zsh/rc.d/05-plugins.zsh
@@ -9,9 +9,11 @@ znap source zsh-users/zsh-completions
 znap source chrissicool/zsh-256color
 znap source zdharma-continuum/fast-syntax-highlighting
 
-ZSH_AUTOSUGGEST_STRATEGY=(history)
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=240'
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(bracketed-paste accept-line)
+ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(widget::key-ctrl-right)
 znap source zsh-users/zsh-autosuggestions
 
 # zstyle ':autocomplete:*' fzf-completion yes
