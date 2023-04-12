c9649e6e876f2c13f8054f6bab328a54e3ca8820
twobiers <22715034+twobiers@users.noreply.github.com>
2023-04-12 22:07:34 +0200

    fix flux

--- dot_config/zsh/rc.d/05-plugins.zsh
+++ dot_config/zsh/rc.d/05-plugins.zsh
@@ -2,7 +2,7 @@
 
 znap source ohmyzsh/ohmyzsh \
   lib/{clipboard,compfix,completion,correction,directories,git,grep,history,spectrum,termsupport} \
-  plugins/{aws,fluxcd,fzf,git,gpg-agent}
+  plugins/{aws,fzf,git,gpg-agent}
 
 znap source zsh-users/zsh-completions
 
--- dot_config/zsh/rc.d/19-kube.zsh
+++ dot_config/zsh/rc.d/19-kube.zsh
@@ -1,1 +1,3 @@
 export PATH="${PATH}:${HOME}/.krew/bin"
+
+znap eval flux 'source <(flux completion zsh)'
