556b66fed8ed297dc2e9ad0e86af60687d7c0aee
twobiers <22715034+twobiers@users.noreply.github.com>
2023-10-17 13:08:33 +0200

    nvm

--- dot_config/zsh/rc.d/16-node.zsh
+++ dot_config/zsh/rc.d/16-node.zsh
@@ -2,6 +2,10 @@
 
 export NVM_COMPLETION=true
 export NVM_SYMLINK_CURRENT="true"
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+
 
 if test -f /usr/share/nvm/init-nvm.sh; then
   source /usr/share/nvm/init-nvm.sh
