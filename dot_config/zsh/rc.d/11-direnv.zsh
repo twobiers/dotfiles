6a422cf4b576c85298b965344a1f143db1626d00
twobiers <22715034+twobiers@users.noreply.github.com>
2023-10-19 08:06:22 +0200

    update for mac

--- dot_config/zsh/rc.d/11-direnv.zsh
+++ dot_config/zsh/rc.d/11-direnv.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-znap eval direnv "direnv hook zsh"
+# znap eval direnv "direnv hook zsh"
 
-# if [[ -x direnv ]]; then
-#   eval "$(direnv hook zsh)"
-# fi
+if [[ -x direnv ]]; then
+  eval "$(direnv hook zsh)"
+fi
