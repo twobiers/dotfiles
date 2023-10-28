a64cc789940bceee5ac251f128a46fb1e2b37f04
twobiers <22715034+twobiers@users.noreply.github.com>
2023-10-28 11:09:43 +0200

    tweak sccripts

--- dot_config/zsh/rc.d/100-mixed.zsh
+++ dot_config/zsh/rc.d/100-mixed.zsh
@@ -1,5 +1,1 @@
 znap eval gh-copilot-cli 'github-copilot-cli alias -- "$0"'
-
-if [ -f "$HOME/.sdkman/bin/sdkman-init.sh" ]; then
-    source "$HOME/.sdkman/bin/sdkman-init.sh"
-fi
--- dot_config/zsh/rc.d/19-kube.zsh
+++
@@ -1,3 +0,0 @@
-export PATH="${PATH}:${HOME}/.krew/bin"
-
-znap eval flux 'source <(flux completion zsh)'
