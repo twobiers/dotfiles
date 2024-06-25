37bd3b4046967d79f6eb2e9bc991b4c0fa93cf7f
twobiers <22715034+twobiers@users.noreply.github.com>
2024-06-25 08:22:10 +0200

    fix

--- dot_config/zsh/rc.d/24-azure.zsh
+++ dot_config/zsh/rc.d/24-azure.zsh
@@ -1,3 +1,3 @@
-$_az_zsh_completer_path="/opt/homebrew/etc/bash_completion.d/az"
+_az_zsh_completer_path="/opt/homebrew/etc/bash_completion.d/az"
 
 [[ -r $_az_zsh_completer_path ]] && autoload -U +X bashcompinit && bashcompinit && source $_az_zsh_completer_path
