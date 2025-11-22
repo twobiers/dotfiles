ebae237f64b5c5d1a7df2973441762c635c5d6a4
twobiers <22715034+twobiers@users.noreply.github.com>
2025-11-22 11:36:57 +0100

    fix nvm

--- dot_config/zsh/rc.d/16-node.zsh
+++ dot_config/zsh/rc.d/16-node.zsh
@@ -5,11 +5,8 @@ export NVM_SYMLINK_CURRENT="true"
 export NVM_DIR="$HOME/.nvm"
 
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-
-
-if test -f /usr/share/nvm/init-nvm.sh; then
-  source /usr/share/nvm/init-nvm.sh
-fi
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && znap eval nvm "source $NVM_DIR/bash_completion"
 
 if test -f $HOME/.bun/bin/bun; then
   export PATH="$PATH:$HOME/.bun/bin/"
