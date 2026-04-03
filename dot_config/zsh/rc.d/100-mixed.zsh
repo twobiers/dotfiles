2588a2cc25575102810a3a0ca6bacf1a60286f7c
twobiers <22715034+twobiers@users.noreply.github.com>
2026-04-03 17:23:20 +0200

    upodate

--- dot_config/zsh/rc.d/100-mixed.zsh
+++ dot_config/zsh/rc.d/100-mixed.zsh
@@ -1,5 +1,10 @@
-eval "$(gh copilot alias -- zsh)"
-eval "$(just --completions zsh)"
+if type "gh" &> /dev/null; then
+  eval "$(gh copilot alias -- zsh)"
+fi
+
+if type "just" &> /dev/null; then
+  eval "$(just --completions zsh)"
+fi
 
 if type "logcli" &> /dev/null; then
   znap eval logcli 'logcli --completion-script-zsh'
