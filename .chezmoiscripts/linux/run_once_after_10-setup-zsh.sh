b00010e3e9849e25fef48848ae96e0356672b147
twobiers <22715034+twobiers@users.noreply.github.com>
2026-05-14 16:47:32 +0200

    Change script shebang to bash and enable strict mode for Zsh setup

--- .chezmoiscripts/linux/run_once_after_10-setup-zsh.sh
+++ .chezmoiscripts/linux/run_once_after_10-setup-zsh.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+set -eufo pipefail
 
 echo "\033[0;32m>>>>> Begin Setting Up Zsh <<<<<\033[0m"
 
 export SHELL=/usr/bin/zsh
-
-
+chsh -s /usr/bin/zsh
 
 echo "\033[0;32m>>>>> Finish Setting Up Zsh <<<<<\033[0m"
