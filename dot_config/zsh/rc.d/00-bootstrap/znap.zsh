b1b08f86de96ae22991c327bc09fbcc491d4ed38
twobiers <22715034+twobiers@users.noreply.github.com>
2026-05-14 20:55:22 +0200

    Remove Znap download logic from bootstrap script

--- dot_config/zsh/rc.d/00-bootstrap/znap.zsh
+++ dot_config/zsh/rc.d/00-bootstrap/znap.zsh
@@ -1,10 +1,5 @@
 #!/usr/bin/env zsh
 
-# Download Znap, if it's not there yet.
-[[ -r ~/.local/share/znap/znap.zsh ]] ||
-    git clone --depth 1 -- \
-        https://github.com/marlonrichert/zsh-snap.git ~/.local/share/znap
-
 source $HOME/.local/share/znap/znap.zsh  # Start Znap
 
 zstyle ':znap:*' plugins-dir "$HOME/.local/share/zsh/plugins"
