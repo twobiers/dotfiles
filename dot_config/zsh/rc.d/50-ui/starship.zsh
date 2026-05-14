0b6dc3a0f8fe54a579f8990aca651f749c3173b8
twobiers <22715034+twobiers@users.noreply.github.com>
2026-05-14 21:46:38 +0200

    Add starship binary to .chezmoiexternal.toml and update zsh initialization script

--- dot_config/zsh/rc.d/50-ui/starship.zsh
+++ dot_config/zsh/rc.d/50-ui/starship.zsh
@@ -1,9 +1,13 @@
 #!/usr/bin/env zsh
 
-# znap eval starship 'starship init zsh'
-eval "$(starship init zsh)"
-znap prompt
+# eval "$(starship init zsh --print-full-init)"
+# znap prompt
 
-# if [[ `which starship &>/dev/null` != 0 ]]; then
-#   eval "$(starship init zsh)"
-# fi
+# For some reason this doesn't work when using the znap eval, so we cache the output manually.
+# znap eval starship 'starship init zsh --print-full-init'
+local cache=${XDG_CACHE_HOME:-~/.cache}/starship/init.zsh
+[[ -s $cache && $cache -nt =starship ]] || {
+  mkdir -p $cache:h && starship init zsh --print-full-init >| $cache
+}
+source $cache
+znap prompt
