92161cc80f59caa39497f62b7ef53e00f4bb9869
twobiers <22715034+twobiers@users.noreply.github.com>
2026-05-14 16:54:00 +0200

    Refactor configuration templates for Linux compatibility and remove macOS-specific settings

--- dot_config/zsh/rc.d/40-runtimes/node.zsh
+++ dot_config/zsh/rc.d/40-runtimes/node.zsh
@@ -4,7 +4,6 @@ export NVM_COMPLETION=true
 export NVM_SYMLINK_CURRENT="true"
 export NVM_DIR="$HOME/.nvm"
 
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && znap eval nvm "source $NVM_DIR/bash_completion"
 
