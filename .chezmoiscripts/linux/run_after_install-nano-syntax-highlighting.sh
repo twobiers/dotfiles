bea391eeef24925794455ea496ba42c5d5f465a6
twobiers <22715034+twobiers@users.noreply.github.com>
2026-05-15 18:20:39 +0200

    Add installation script for nano syntax highlighting

--- .chezmoiscripts/linux/run_after_install-nano-syntax-highlighting.sh
+++ .chezmoiscripts/linux/run_after_install-nano-syntax-highlighting.sh
@@ -4,5 +4,16 @@ set -eufo pipefail
 # shellcheck source=../../lib/lib.sh
 source "${CHEZMOI_SOURCE_DIR}/lib/lib.sh"
 
+NANORC_FILE="$HOME/.nanorc"
+NANORC_SYNTAX_HIGHLIGHTING_DIR="$HOME/.local/share/nano-syntax-highlighting"
+
 log::info "Installing nano syntax highlighting"
-"$HOME/.local/share/nano-syntax-highlighting/install.sh"
+mkdir -p ~/.nano/
+find $NANORC_SYNTAX_HIGHLIGHTING_DIR -name ".nanorc" -exec cp '{}' ~/.nano/ \; || true
+touch "$NANORC_FILE"
+
+while read -r inc; do
+  if ! grep -q "$inc" "${NANORC_FILE}"; then
+    echo "$inc" >> "$NANORC_FILE"
+  fi
+done < "$NANORC_SYNTAX_HIGHLIGHTING_DIR/nanorc"
